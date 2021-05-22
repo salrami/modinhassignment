f8382159f517cad5fab31193dfda2ed68903a21f
Rami Salame <Rami Salame@192.168.0.10>
2021-05-22 17:37:47 -0400

    new commit

--- src/javaapplication36/JavaApplication36.java
+++ src/javaapplication36/JavaApplication36.java
@@ -5,69 +5,31 @@
  */
 package javaapplication36;
 
-import java.util.Scanner;
-
 public class JavaApplication36 {
-    public double x;
-    public double y;
-    public JavaApplication36() {
-        Scanner input = new Scanner(System.in);
-        x = input.nextDouble();
+        void calculateArea(float x) {
+            System.out.println("Area of the square: "+x*x+" sq units");
+    }
+        
+        void calculateArea(float x, float y) {
+            System.out.println("Area of the rectangle: "+x*y+" sq units");
+    }
+    
+        void calculateArea(double r) {
+            double area = 3.14*r*r;
+            System.out.println("Area of the circle: "+area+" sq units");
+    }
 
     
-}
-
     public static void main(String[] args) {
-         Areas objArea = new Areas();
-         
-         Perimeters objPerimeter = new Perimeters(); 
+         JavaApplication36 obj = new JavaApplication36();
        
-         
-	 objArea.calculateArea();
+	 obj.calculateArea(6.1f);
 	  
-	 objArea.calculateArea();
+	 obj.calculateArea(10,22);
 	   
-	 objArea.calculateArea();
-         
-         objPerimeter.calculatePerimeter();
-	  
-	 objPerimeter.calculatePerimeter();
-	   
-	 objPerimeter.calculatePerimeter();
+	 obj.calculateArea(6.1);
     }
 
     
-
-}
-
-class Areas extends JavaApplication36 {
-    protected void calculateArea(float x) {
-            System.out.println("Area of the square: "+x*x+" sq units");
-    }
-        
-        protected void calculateArea(float x, float y) {
-            System.out.println("Area of the rectangle: "+x*y+" sq units");
-    }
-    
-        protected void calculateArea(double r) {
-            double area = 3.14*r*r;
-            System.out.println("Area of the circle: "+area+" sq units");
-    }
-
-}
-
-class Perimeters extends JavaApplication36 {
-        protected void calculatePerimeter(float x) {
-            System.out.println("Area of the square: "+x*x+" sq units");
-    }
-        
-        protected void calculatePerimeter(float x, float y) {
-            System.out.println("Area of the rectangle: "+x*y+" sq units");
-    }
-    
-        protected void calculatePerimeter(double r) {
-            double area = 3.14*r*r;
-            System.out.println("Area of the circle: "+area+" sq units");
-    }
 
 }
