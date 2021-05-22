b8b8a018525404c12dd25c4cadd221c267b1fe49
Rami Salame <Rami Salame@192.168.0.10>
2021-05-22 17:09:09 -0400

    new commit

--- src/javaapplication36/JavaApplication36.java
+++ src/javaapplication36/JavaApplication36.java
@@ -6,110 +6,68 @@
 package javaapplication36;
 
 import java.util.Scanner;
-//superclass
+
 public class JavaApplication36 {
-    public double length;
-    public double width;
-    public double radius;
-    
-    //encapsulating 
-    protected void getLength(double newValue) {
-	this.length = newValue;
-	}
-    
-    protected void getWidth(double newValue) {
-	this.width = newValue;
-	}
-
-    protected void getRadius(double newValue) {
-	this.radius = newValue;
-	}
-    
-    protected double calcArea() {
-	return 0;
-	}
-    
-    protected double calcPeri() {
-	return 0;
-	}
-    
-    public void output() {
-		System.out.println("The area is " + calcArea());
-		System.out.println("The perimeter is " + calcPeri());
-    }
+    public double x;
+    public double y;
     public JavaApplication36() {
-        length = 0;
-        width = 0;
-        radius = 0;
+        Scanner input = new Scanner(System.in);
+        x = input.nextDouble();
 
+    
 }
 
     public static void main(String[] args) {
-        //instantiation of objects
-        JavaApplication36 obj = new JavaApplication36();
-        Rectangle robj = new Rectangle(); 
-        Circle cobj = new Circle();
-        //checking if user inputs a number 
-        while (true) {
-            try {
-            Scanner input = new Scanner(System.in);
-
-        
-            System.out.print("Enter length: ");
-            double length_value = input.nextDouble();
-            robj.getLength(length_value);
-        
-            System.out.print("Enter width: ");
-            double width_value = input.nextDouble();
-            robj.getWidth(width_value);
-        
-            System.out.print("Enter radius: ");
-            double radius_value = input.nextDouble();
-            cobj.getRadius(radius_value);
-        
-            System.out.println("The output of your rectangle is: ");
-            robj.output();
-            System.out.println("The output of your circle is: ");
-            cobj.output();
-            break;
-           
-        } catch (Exception e) {
-            System.out.println("Enter a number");
-            
-        }
-            
-       }
-        
+         Areas objArea = new Areas();
+         
+         Perimeters objPerimeter = new Perimeters(); 
+       
+         
+	 objArea.calculateArea();
+	  
+	 objArea.calculateArea();
+	   
+	 objArea.calculateArea();
+         
+         objPerimeter.calculatePerimeter();
+	  
+	 objPerimeter.calculatePerimeter();
+	   
+	 objPerimeter.calculatePerimeter();
     }
+
+    
 
 }
-//subclasses for shapes
-class Rectangle extends JavaApplication36 {
-    public double calcArea() {
-        double area = Math.round(length*width);
-        return area;
-        
+
+class Areas extends JavaApplication36 {
+    protected void calculateArea(float x) {
+            System.out.println("Area of the square: "+x*x+" sq units");
     }
         
-    public double calcPeri() {
-        double perimeter = Math.round(2*length + 2*width);
-        return perimeter;
-        
+        protected void calculateArea(float x, float y) {
+            System.out.println("Area of the rectangle: "+x*y+" sq units");
+    }
+    
+        protected void calculateArea(double r) {
+            double area = 3.14*r*r;
+            System.out.println("Area of the circle: "+area+" sq units");
     }
 
 }
 
-class Circle extends JavaApplication36 {
-    public double calcArea() {
-        double area = Math.round(Math.PI*(radius*radius));
-        return area;
-        
+class Perimeters extends JavaApplication36 {
+        protected void calculatePerimeter(float x) {
+            System.out.println("Area of the square: "+x*x+" sq units");
     }
         
-    public double calcPeri() {
-        double perimeter = Math.round(2*Math.PI*radius);
-        return perimeter;
-        
+        protected void calculatePerimeter(float x, float y) {
+            System.out.println("Area of the rectangle: "+x*y+" sq units");
+    }
+    
+        protected void calculatePerimeter(double r) {
+            double area = 3.14*r*r;
+            System.out.println("Area of the circle: "+area+" sq units");
     }
 
 }
